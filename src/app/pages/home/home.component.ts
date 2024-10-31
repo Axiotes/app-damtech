95b4bccff85277a38d5fdc9bebcfd1e15de9ea08
Arthur Axiotes <axiotesarthur@gmail.com>
2024-10-30 23:25:00 -0300

    feat: adicionar rotas

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { LucideAngularModule, MapPin, SendHorizontal } from 'lucide-angular';
 import { LucideIconData } from 'lucide-angular/icons/types';
+import { InfosComponent } from '../../components/infos/infos.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, InfosComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
