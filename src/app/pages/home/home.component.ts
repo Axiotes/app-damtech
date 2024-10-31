671703a5a821f2c01530aa44810769a7b80aa7bc
Arthur Axiotes <axiotesarthur@gmail.com>
2024-10-31 00:53:29 -0300

    feat: componente ja pode ser alternado entre empresas e usuarios comuns

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,51 @@
-import { Component } from '@angular/core';
-import { LucideAngularModule, MapPin, SendHorizontal } from 'lucide-angular';
+import { Component, OnInit } from '@angular/core';
+import {
+  Building2,
+  LucideAngularModule,
+  MapPin,
+  SendHorizontal,
+} from 'lucide-angular';
 import { LucideIconData } from 'lucide-angular/icons/types';
 import { InfosComponent } from '../../components/infos/infos.component';
+import { DescriptionHome } from '../../../types/description-home.type';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [LucideAngularModule, InfosComponent],
+  imports: [LucideAngularModule, InfosComponent, NgIf],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   public mapPin: LucideIconData = MapPin;
   public sendHorizontal: LucideIconData = SendHorizontal;
+  public building2: LucideIconData = Building2;
+
+  public description!: DescriptionHome;
+  public currentUser: string = 'all-users';
+
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit(): void {
+    this.route.url.subscribe((url) => {
+      this.currentUser = url[1].path;
+    });
+
+    const description: { [key: string]: DescriptionHome } = {
+      'all-users': {
+        title: 'Seja bem vindo ao DamTech!',
+        text: 'Com o propósito de garantir sua segurança e manter você informados sobre barragens em sua região.',
+        filter: 'Insira sua cidade e/ou estado:',
+      },
+      'admin-users': {
+        title: 'Seja bem vindo ao DamTech para empresas',
+        text: 'Destinado a empresas de fiscalização para garantir a proteção e a segurança das barragens.',
+        filter: 'Insira a cidade e estado:',
+      },
+    };
+
+    this.description = description[this.currentUser];
+  }
 }
