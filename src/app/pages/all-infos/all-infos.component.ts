b642dfb6c192120fe5cb24b63310deeeb915fcd1
Arthur Axiotes <axiotesarthur@gmail.com>
2024-10-31 08:46:24 -0300

    feat: criacao da rota para todas as informacoes

---
+++ src/app/pages/all-infos/all-infos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-infos',
+  standalone: true,
+  imports: [],
+  templateUrl: './all-infos.component.html',
+  styleUrl: './all-infos.component.scss'
+})
+export class AllInfosComponent {
+
+}
