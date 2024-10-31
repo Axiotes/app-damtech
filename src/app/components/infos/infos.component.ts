8c2d339f93668943d19c2fc08135d994e348e6c3
Arthur Axiotes <axiotesarthur@gmail.com>
2024-10-30 21:36:05 -0300

    feat: abrir e fechar as infos

--- src/app/components/infos/infos.component.ts
+++ src/app/components/infos/infos.component.ts
@@ -1,3 +1,4 @@
+import { NgClass, NgIf, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 import { ChevronDown, Dam, History, LucideAngularModule } from 'lucide-angular';
 import { LucideIconData } from 'lucide-angular/icons/types';
@@ -5,12 +6,34 @@ import { LucideIconData } from 'lucide-angular/icons/types';
 @Component({
   selector: 'app-infos',
   standalone: true,
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, NgStyle, NgIf, NgClass],
   templateUrl: './infos.component.html',
   styleUrl: './infos.component.scss',
 })
 export class InfosComponent {
   public dam: LucideIconData = Dam;
   public chevronDown: LucideIconData = ChevronDown;
-  public history: LucideIconData = History
+  public history: LucideIconData = History;
+
+  public height: number = 175;
+  public opacity: number = 1;
+  public margin: number = 0;
+  public isOpened: boolean = true;
+
+
+  public toogleHeight(): void {
+    this.isOpened = !this.isOpened;
+
+    if (this.isOpened) {
+      this.height = 175;
+      this.opacity = 1;
+      this.margin = 0;
+
+      return;
+    }
+
+    this.height = 60;
+    this.opacity = 0;
+    this.margin = 20;
+  }
 }
