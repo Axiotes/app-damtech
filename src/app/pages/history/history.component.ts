df1bd49289624ec25c1ffd48fe2fde4a78c93129
Arthur Axiotes <axiotesarthur@gmail.com>
2024-10-31 22:19:51 -0300

    feat: criacao do componente e estilizacao

---
+++ src/app/pages/history/history.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Dam, LucideAngularModule, MoveLeft } from 'lucide-angular';
+import { LucideIconData } from 'lucide-angular/icons/types';
+
+@Component({
+  selector: 'app-history',
+  standalone: true,
+  imports: [LucideAngularModule],
+  templateUrl: './history.component.html',
+  styleUrl: './history.component.scss'
+})
+export class HistoryComponent implements OnInit {
+  public idDam!: number;
+
+  public moveLeft: LucideIconData = MoveLeft;
+  public dam: LucideIconData = Dam;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.idDam = params['id_dam'];
+    });
+  }
+}
